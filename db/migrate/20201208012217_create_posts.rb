f5c17c4de39822176d47fbbce573c7eb4155d594
Sean McEvoy <sfmcevoy@gmail.com>
2020-12-07 19:26:50 -0600

    Add validations and constraints to Post

--- db/migrate/20201208012217_create_posts.rb
+++ db/migrate/20201208012217_create_posts.rb
@@ -1,10 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.string :username
+      t.string :username, null: false, default: "Sean"
       t.string :body
-      t.integer :likes_count
-      t.integer :reposts_count
+      t.integer :likes_count, null: false, default: 0
+      t.integer :reposts_count, null: false, default: 0
 
       t.timestamps
     end
