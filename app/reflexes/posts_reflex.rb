32c3cd1ef5f7b14c6f7890ccf282adb7da0d962f
Sean McEvoy <sfmcevoy@gmail.com>
2020-12-07 20:34:39 -0600

    Basic reflex actions

--- app/reflexes/posts_reflex.rb
+++ app/reflexes/posts_reflex.rb
@@ -1,25 +1,13 @@
 # frozen_string_literal: true
 
 class PostsReflex < ApplicationReflex
-  # Add Reflex methods in this file.
-  #
-  # All Reflex instances expose the following properties:
-  #
-  #   - connection - the ActionCable connection
-  #   - channel - the ActionCable channel
-  #   - request - an ActionDispatch::Request proxy for the socket connection
-  #   - session - the ActionDispatch::Session store for the current visitor
-  #   - url - the URL of the page that triggered the reflex
-  #   - element - a Hash like object that represents the HTML element that triggered the reflex
-  #   - params - parameters from the element's closest form (if any)
-  #
-  # Example:
-  #
-  #   def example(argument=true)
-  #     # Your logic here...
-  #     # Any declared instance variables will be made available to the Rails controller and view.
-  #   end
-  #
-  # Learn more at: https://docs.stimulusreflex.com
+  def repost
+    post = Post.find(element.dataset[:id])
+    post.increment! :reposts_count
+  end
 
+  def like
+    post = Post.find(element.dataset[:id])
+    post.increment! :likes_count
+  end
 end
